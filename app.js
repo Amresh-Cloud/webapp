01f3ad12c0e394ba2de4fdb2616e3a468a3f07fd
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-02-07 14:57:52 -0500

    added .yml

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const sequelize = require("../webapp-fork1/DatabaseConnection/connection");
 const routes=require("../webapp-fork1/RouteHandler/routes");
+const dbCheck = require("../webapp-fork1/Middleware/dbcheck");
 
 
 const port = 1000;
+app.use("/v1",dbCheck);
 app.use(routes);
 
 app.listen(port, () => {
