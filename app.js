d32e997787ee4d7a4c160cbf15d1004d89ab5967
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-02-21 14:57:37 -0500

    port number updated

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const routes=require("./RouteHandler/routes");
 const dbCheck = require("./Middleware/dbcheck");
 
 
-const port = 3389;
+const port = 2500;
 app.use("/v1",dbCheck);
 app.use(routes);
 
 app.listen(port, () => {
-  sequelize.sync({force:true}).then(console.log("Server at port 3389"));
+  sequelize.sync({force:true}).then(console.log("Server at port 2500"));
 });
 module.exports=app;
--- packer/packer.pkr.hcl
+++ packer/packer.pkr.hcl
@@ -4,7 +4,7 @@ variable "disk_size" {
 }
 variable "image_name" {
   type    = string
-  default = "custom-image-3"
+  default = "custom-image-4"
 }
 
 variable "project_id" {
