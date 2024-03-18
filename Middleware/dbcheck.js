0b57cc3d668784ed0e82ea45222ef7c3e417a2fe
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-03-18 14:23:45 -0400

    added logger

--- Middleware/dbcheck.js
+++ Middleware/dbcheck.js
@@ -1,9 +1,11 @@
 const sequelize = require("../DatabaseConnection/connection");
+const logger=require("../app");
 const dbCheck = async (req, res, next) => {
   try {
     await sequelize.authenticate();
     next();
   } catch (error) {
+    logger.error("Database Not Connected", error);
     console.log("Database Not Connected");
     res.header("Cache-Control", "no-cache");
     res.status(503).end();
--- app.js
+++ app.js
@@ -3,13 +3,35 @@ const app = express();
 const sequelize = require("./DatabaseConnection/connection");
 const routes=require("./RouteHandler/routes");
 const dbCheck = require("./Middleware/dbcheck");
+const winston = require('winston');
+const {LoggingWinston} = require('@google-cloud/logging-winston');
 
 
+const loggingWinston = new LoggingWinston();
+
+// Create a Winston logger that streams to Cloud Logging
+// Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
+const logger = winston.createLogger({
+  level: 'info',
+transports: [
+  new winston.transports.Console(),
+  loggingWinston,
+ 
+],
+});
+
+
+
+// Writes some log entries
+logger.error('warp nacelles offline');
+logger.info('shields at 99%');
 const port = process.env.PORT || 2500;
 app.use("/v1",dbCheck);
 app.use(routes);
 
 app.listen(port, () => {
   sequelize.sync({force:true}).then(console.log('Server at port',port));
+
 });
 module.exports=app;
+module.exports=logger;
