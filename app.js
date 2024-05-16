0e949475c687dc0491638c084a83fc02c9cef450
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2024-05-16 08:51:07 +0530

    corrected all the routes and controllers

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const sequelize = require("./DatabaseConnection/connection");
 const routes=require("./RouteHandler/routes");
 const dbCheck = require("./Middleware/dbcheck");
+const methodNotAllowed= require("./Middleware/methodsNotAllowed");
 const logger=require("./utils/logger");
 const port = process.env.PORT || 2500;
 app.use("/v2",dbCheck);
+app.use(methodNotAllowed);
 app.use(routes);
 
 app.listen(port, () => {
